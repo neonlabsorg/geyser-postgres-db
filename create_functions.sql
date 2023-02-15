b785a4f7a6d5812d916a6c956d5b0d1945ddc3e9
Ivan Loboda <loboda.ivan.y@gmail.com>
2023-02-15 22:17:03 +0400

    Fix error in SP

--- create_functions.sql
+++ create_functions.sql
@@ -231,6 +231,100 @@ END;
 $get_pre_accounts_branch$ LANGUAGE plpgsql;
 
 -----------------------------------------------------------------------------------------------------------------------
+-- Returns latest versions of accounts with pubkeys included in @transaction_accounts
+-- with update events in slots not much than @start_slot
+-- and write versions not much than @max_write_version
+-- Search is performed over account_audit table (latest written history)
+CREATE OR REPLACE FUNCTION get_pre_audit_accounts(
+    start_slot BIGINT,
+    max_write_version BIGINT,
+    transaction_accounts BYTEA[]
+)
+
+RETURNS TABLE (
+    lamports BIGINT,
+    data BYTEA,
+    owner BYTEA,
+    executable BOOL,
+    rent_epoch BIGINT,
+    pubkey BYTEA,
+    slot BIGINT,
+    write_version BIGINT,
+    signature BYTEA
+)
+
+AS $get_pre_audit_accounts$
+
+BEGIN
+    RETURN QUERY
+        SELECT DISTINCT ON (acc.pubkey)
+            acc.lamports,
+            acc.data,
+            acc.owner,
+            acc.executable,
+            acc.rent_epoch,
+            acc.pubkey,
+            acc.slot,
+            acc.write_version,
+            acc.txn_signature
+        FROM public.account_audit AS acc
+        WHERE
+            acc.pubkey IN (SELECT * FROM unnest(transaction_accounts))
+            AND acc.slot <= start_slot
+            AND acc.write_version < max_write_version
+        ORDER BY
+            acc.pubkey DESC,
+            acc.slot DESC,
+            acc.write_version DESC;
+END;
+$get_pre_audit_accounts$ LANGUAGE plpgsql;
+
+-----------------------------------------------------------------------------------------------------------------------
+-- Returns latest versions of accounts with pubkeys included in @transaction_accounts
+-- with update events in slots not much than @start_slot
+-- and write versions not much than @max_write_version
+-- Search is performed over older_account table (oldest versions)
+CREATE OR REPLACE FUNCTION get_pre_older_accounts(
+    start_slot BIGINT,
+    max_write_version BIGINT,
+    transaction_accounts BYTEA[]
+)
+
+RETURNS TABLE (
+    lamports BIGINT,
+    data BYTEA,
+    owner BYTEA,
+    executable BOOL,
+    rent_epoch BIGINT,
+    pubkey BYTEA,
+    slot BIGINT,
+    write_version BIGINT,
+    signature BYTEA
+)
+
+AS $get_pre_older_accounts$
+
+BEGIN
+    RETURN QUERY
+        SELECT
+            old.lamports,
+            old.data,
+            old.owner,
+            old.executable,
+            old.rent_epoch,
+            old.pubkey,
+            old.slot,
+            old.write_version,
+            old.txn_signature
+        FROM public.older_account AS old
+        WHERE
+            old.pubkey IN (SELECT * FROM unnest(transaction_accounts))
+            AND old.slot <= start_slot
+            AND old.write_version < max_write_version;
+END;
+$get_pre_older_accounts$ LANGUAGE plpgsql;
+
+-----------------------------------------------------------------------------------------------------------------------
 CREATE OR REPLACE FUNCTION get_pre_accounts_root(
     start_slot BIGINT,
     max_write_version BIGINT,
@@ -253,24 +347,19 @@ AS $get_pre_accounts_root$
 
 BEGIN
     RETURN QUERY
-        SELECT DISTINCT ON (acc.pubkey)
-            acc.lamports,
-            acc.data,
-            acc.owner,
-            acc.executable,
-            acc.rent_epoch,
-            acc.pubkey,
-            acc.slot,
-            acc.write_version,
-            acc.txn_signature
-        FROM public.account_audit AS acc
-        WHERE
-            acc.slot <= start_slot
-            AND acc.write_version < max_write_version
-            AND acc.pubkey IN (SELECT * FROM unnest(transaction_accounts))
+        -- root branch consist of historical states in account_audit 
+        -- plus olderst available state in older_accounts - collect from both
+        -- and take only latest for each account from transaction_accounts
+        WITH results AS (
+            SELECT * FROM get_pre_audit_accounts(start_slot, max_write_version, transaction_accounts)
+            UNION
+            SELECT * FROM get_pre_older_accounts(start_slot, max_write_version, transaction_accounts)
+        )
+        SELECT DISTINCT ON (res.pubkey) * FROM results AS res 
         ORDER BY
-            acc.pubkey DESC,
-            acc.write_version DESC;
+            res.pubkey DESC,
+            res.slot DESC,
+            res.write_version DESC;
 END;
 $get_pre_accounts_root$ LANGUAGE plpgsql;
 
@@ -386,6 +475,49 @@ END;
 $get_pre_accounts$ LANGUAGE plpgsql;
 
 -----------------------------------------------------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION get_account_at_slot_from_audit(
+    in_pubkey BYTEA,
+    in_slot BIGINT
+)
+
+RETURNS TABLE (
+    pubkey BYTEA,
+	owner BYTEA,
+	lamports BIGINT,
+	executable BOOL,
+	rent_epoch BIGINT,
+	data BYTEA,
+    slot BIGINT,
+    write_version BIGINT,
+    signature BYTEA
+)
+
+AS $get_account_at_slot_from_audit$
+
+BEGIN
+    RETURN QUERY
+        SELECT DISTINCT ON (acc.pubkey)
+            acc.pubkey,
+            acc.owner,
+            acc.lamports,
+            acc.executable,
+            acc.rent_epoch,
+            acc.data,
+            acc.slot,
+            acc.write_version,
+            acc.txn_signature
+        FROM public.account_audit AS acc
+        WHERE
+            acc.slot <= in_slot
+            AND acc.pubkey = in_pubkey
+        ORDER BY 
+            acc.pubkey DESC, 
+            acc.write_version DESC 
+        LIMIT 1;
+END;
+$get_account_at_slot_from_audit$ LANGUAGE plpgsql;
+
+-----------------------------------------------------------------------------------------------------------------------
 CREATE OR REPLACE FUNCTION get_account_at_slot(
     in_pubkey BYTEA,
     in_slot BIGINT
@@ -407,22 +539,25 @@ AS $get_account_at_slot$
 
 BEGIN
     RETURN QUERY
-        SELECT
-            acc.pubkey,
-            acc.owner,
-            acc.lamports,
-            acc.executable,
-            acc.rent_epoch,
-            acc.data,
-            acc.slot,
-            acc.write_version,
-            acc.txn_signature
-        FROM public.account_audit AS acc
-        WHERE
-            acc.slot <= in_slot
-            AND acc.pubkey = in_pubkey
-        ORDER BY
-            acc.slot DESC, acc.write_version DESC
+        WITH results AS (
+            SELECT * FROM get_account_at_slot_from_audit(in_pubkey, in_slot)
+            UNION
+            SELECT
+                old.pubkey,
+                old.owner,
+                old.lamports,
+                old.executable,
+                old.rent_epoch,
+                old.data,
+                old.slot,
+                old.write_version,
+                old.txn_signature
+            FROM public.older_account AS old
+            WHERE
+                old.pubkey = in_pubkey
+        )
+        SELECT * FROM results AS res
+        ORDER BY res.slot DESC, res.write_version DESC 
         LIMIT 1;
 END;
 $get_account_at_slot$ LANGUAGE plpgsql;
@@ -511,3 +646,36 @@ BEGIN
         acc1.slot < until_slot;
 END;
 $update_older_account$ LANGUAGE plpgsql;
+
+-----------------------------------------------------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION get_recent_update_slot(
+    for_pubkey BYTEA,
+    until_slot BIGINT
+)
+
+RETURNS TABLE (
+	slot BIGINT
+)
+
+AS $get_recent_update_slot$
+
+BEGIN
+    RETURN QUERY
+        WITH results AS (
+            SELECT acc.slot, acc.write_version 
+            FROM public.account_audit AS acc
+            WHERE 
+                acc.pubkey = for_pubkey 
+                AND acc.slot <= until_slot 
+            UNION
+            SELECT old_acc.slot, old_acc.write_version
+            FROM public.older_account AS old_acc
+            WHERE
+                old_acc.pubkey = for_pubkey
+        )
+        SELECT res.slot
+        FROM results AS res
+        ORDER BY res.slot DESC, res.write_version DESC
+        LIMIT 1;
+END;
+$get_recent_update_slot$ LANGUAGE plpgsql;
