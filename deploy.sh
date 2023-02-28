835a40b8b586a05fd03267763a51466cbdd397e4
Ivan Loboda <loboda.ivan.y@gmail.com>
2023-02-28 13:05:22 +0400

    pg_cron schedule ready

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,16 @@ if [ -z "$PGPASSWORD" ]; then
    PGPASSWORD=solana-pass
 fi
 
+cat <<EOT >> ${PGDATA}/postgresql.conf
+shared_preload_libraries = 'pg_cron'
+EOT
+
+cat <<EOT >> ${PGDATA}/postgresql.conf
+cron.database_name='${POSTGRES_DB:-solana}'
+EOT
+
+pg_ctl restart
+
 psql \
   --dbname=solana \
   --username=$POSTGRES_USER \
