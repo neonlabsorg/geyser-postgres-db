d767096197e292730f353c454a9567577d15bfe6
Ivan Loboda <loboda.ivan.y@gmail.com>
2023-03-17 11:42:42 +0400

    reconfigure DB

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,14 @@ cat <<EOT >> ${PGDATA}/postgresql.conf
 cron.database_name='${POSTGRES_DB:-solana}'
 EOT
 
+cat <<EOT >> ${PGDATA}/postgresql.conf
+max_wal_size=8GB
+EOT
+
+cat <<EOT >> ${PGDATA}/postgresql.conf
+shared_buffers=1GB
+EOT
+
 echo "Restarting PostgreSQL server..."
 pg_ctl restart
 
